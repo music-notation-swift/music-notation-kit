4b940c0a0396ba6661382707501693a34a01f7d6
Miguel Osorio <migue48@gmail.com>
2017-03-10 19:41:37 -0800

    Code review cleanup

--- Sources/MusicNotationKit/FileIO/Protobuf/_Interval+Transform.swift
+++ Sources/MusicNotationKit/FileIO/Protobuf/_Interval+Transform.swift
@@ -3,14 +3,14 @@ import MusicNotationCore
 extension _Interval {
     func transformQuality() -> MusicNotationCore.IntervalQuality {
         switch self.quality {
-        case .augmented: return MusicNotationCore.IntervalQuality.augmented
-        case .diminished: return MusicNotationCore.IntervalQuality.diminished
-        case .doublyAugmented: return MusicNotationCore.IntervalQuality.doublyAugmented
-        case .doublyDiminished: return MusicNotationCore.IntervalQuality.doublyDiminished
-        case .major: return MusicNotationCore.IntervalQuality.major
-        case .minor: return MusicNotationCore.IntervalQuality.minor
-        case .perfect: return MusicNotationCore.IntervalQuality.perfect
-        default: return MusicNotationCore.IntervalQuality.major
+        case .augmented: return .augmented
+        case .diminished: return .diminished
+        case .doublyAugmented: return .doublyAugmented
+        case .doublyDiminished: return .doublyDiminished
+        case .major: return .major
+        case .minor: return .minor
+        case .perfect: return .perfect
+        default: return .major
         }
     }
     func transform() throws -> Interval {
